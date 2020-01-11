0f68f809d942c3e8243fb3d52f29f3acb05a4cae
Diego G Campana <diegogcampana@hotmail.com>
2020-01-11 00:34:41 -0600

    update

--- src/main/java/com/kmp/site/travelweb/application/configuration/SpringSecurityConfig.java
+++ src/main/java/com/kmp/site/travelweb/application/configuration/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -30,7 +31,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.sessionManagement()
+    		.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+    		.and().authorizeRequests()
 				.antMatchers("/application/**","/js/**","/css/**","/img/**","/fonts/**","/vendor/**","/webjars/**").permitAll()
 				.antMatchers("/", "/register", "/session/confirmation-email").permitAll()
 				.antMatchers("/tours","/adventure","/gastronomy", "/detail", "/search/**").permitAll()
